b4589a9548245af16e3b2add3ca85973dc31397e
LeeGunhyoung <yuby1003@naver.com>
2023-01-02 19:59:12 +0900

    0102

--- myapp/app/src/main/java/bitcamp/myapp/Main.java
+++ myapp/app/src/main/java/bitcamp/myapp/Main.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int  A, B;
-    A = scanner.nextInt();
-    B = scanner.nextInt();
 
-    while(B > 0) {
-      sum += B%10;
-      B /= 10;
+    int A = scanner.nextInt();
+    int B = scanner.nextInt();
+    int C = scanner.nextInt();
+    scanner.close();
+    int max;
+    if (A>=B && A>=B) {
+      max = A;
+    } else if(B>=A && B>=C) {
+      max = B;
+    } else {
+      max = C;
     }
-    System.out.println(B);
+
+
+    if(A == B && A == C) {
+      System.out.print(10000 + (A * 1000));
+    } else if (A != B && A != C && B != C) {
+      System.out.print(max);
+    } else if()
+
 
   }
 }
