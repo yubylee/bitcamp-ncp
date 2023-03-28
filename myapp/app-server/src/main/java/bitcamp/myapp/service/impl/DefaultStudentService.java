bd9db03b04956c97826148202164b4450a999d08
LeeGunhyoung <yuby1003@naver.com>
2023-03-28 09:49:18 +0900

    '.'

--- myapp/app-server/src/main/java/bitcamp/myapp/service/impl/DefaultStudentService.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/service/impl/DefaultStudentService.java
@@ -43,6 +43,12 @@ public class DefaultStudentService implements StudentService {
     return studentDao.findByEmailAndPassword(paramMap);
   }
 
+  @Override
+  public Student get(String email) {
+    return studentDao.findByEmail(email);
+  }
+
+
   @Transactional
   @Override
   public void update(Student student) {
